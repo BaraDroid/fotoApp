ff2ad4f371ca1b11bdf96385f71363f147391a59
BaraDroid <lambeinovab@seznam.cz>
2025-08-30 07:30:50 +0200

    array with fotos added

---
+++ data.js
@@ -0,0 +1,58 @@
+const savedFotos = [
+  {
+    path: "assets/imgs/chinese-533081_1280.jpg",
+    name: "Berge mit Wolken",
+  },
+  {
+    path: "assets/imgs/eve-9726477_1280.jpg",
+    name: "Steininsel im Wasser",
+  },
+  {
+    path: "assets/imgs/field-6574455_1280.jpg",
+    name: "Feld im Nebel",
+  },
+  {
+    path: "assets/imgs/forest-7604096_1280.jpg",
+    name: "Wald im Winter",
+  },
+  {
+    path: "assets/imgs/horses-9057950_1280.jpg",
+    name: "Pferde auf der Wiese",
+  },
+  {
+    path: "assets/imgs/ocean-1867285_1280.jpg",
+    name: "Nahaufnahme Welle",
+  },
+  {
+    path: "assets/imgs/pontoon-941050_1280.jpg",
+    name: "Ponton",
+  },
+  {
+    path: "assets/imgs/scenery-664855_1280.jpg",
+    name: "Feld mit Tau",
+  },
+  {
+    path: "assets/imgs/sea-9455144_1280.jpg",
+    name: "See mit Boot",
+  },
+  {
+    path: "assets/imgs/sunset-404072_1280.jpg",
+    name: "Sonnenuntergang im Nebel",
+  },
+  {
+    path: "assets/imgs/sunset-4845088_1280.jpg",
+    name: "Roter Sonnenuntergang",
+  },
+  {
+    path: "assets/imgs/tree-4222480_1280.jpg",
+    name: "Baum mit lila Filter",
+  },
+  {
+    path: "assets/imgs/winter-landscape-2571788_1280.jpg",
+    name: "Wegen Nebel kaum sichtbare Bäume",
+  },
+  {
+    path: "assets/imgs/wood-9616079_1280.jpg",
+    name: "Wald auf steilem Berg",
+  },
+];
--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const dialogEl = document.getElementById("detailDialog");
+const mainEl = document.querySelector(".main_section");
+
+document.addEventListener('DOMContentLoaded', renderFotos);
 
 function showDialog() {
     dialogEl.showModal();
+}
+
+function renderFotos() {
+    for (let index = 0; index < savedFotos.length; index++) {
+        const element = array[index];
+        let imgTag = '';
+        imgTag = `<img src="" alt="">`;
+        mainEl.innerHTML += imgTag;
+    }
 }
