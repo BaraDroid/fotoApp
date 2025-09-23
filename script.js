ef6b495cf1e665c9bf17c28151d05d497b81c264
BaraDroid <lambeinovab@seznam.cz>
2025-09-23 06:29:18 +0200

    fix dialog for only open by javascript trigger

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ function showDialog() {
     dialogEl.showModal();
 }
 
+function closeDialog() {
+    dialogEl.close();
+}
+
 function renderFotos() {
     for (let index = 0; index < savedFotos.length; index++) {
         const element = savedFotos[index];
         let imgTag = '';
-        imgTag = `<div class="image_container">
+        imgTag = `<div class="image_container" aria-haspopup="dialog" aria-controls="dialogEl" onclick="showDialog()">
         <img src="${element.path}" alt="${element.name}" class="image_container__image">
         </div>`;
         mainEl.innerHTML += imgTag;
